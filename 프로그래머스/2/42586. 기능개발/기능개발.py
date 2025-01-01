924f58b1f0805ebba77eb2bde9f1aa00344967a8
yeahhh1e <148204348+yeahhh1e@users.noreply.github.com>
2025-01-01 20:12:15 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -0,0 +1,25 @@
+import math
+
+def solution(progresses, speeds):
+    # 각 작업이 완료되기까지 남은 일수를 계산하여 리스트에 저장
+    left_days = [math.ceil((100 - progresses[i]) / speeds[i]) for i in range(len(progresses))]
+    
+    cur_day = left_days[0]  # 현재 배포 기준이 되는 날
+    head = 0  # 처리 중인 작업의 시작 인덱스
+    answer = []  # 결과를 저장할 리스트
+
+    while head < len(left_days):  # 모든 작업을 처리할 때까지 반복
+        temp = 0  # 이번 배포에서 완료될 작업 수를 저장
+
+        for i in range(head, len(left_days)):  # 현재 인덱스부터 작업 확인
+            if cur_day < left_days[i]:  # 이번 작업이 기준일보다 더 오래 걸릴 경우
+                cur_day = left_days[i]  # 배포 기준일을 갱신
+                break  # 현재 배포 종료
+
+            temp += 1  # 배포 가능한 작업 수 증가
+            head += 1  # 처리한 작업 인덱스 이동
+
+    
+        answer.append(temp)  # 결과 리스트에 추가
+    
+    return answer  # 결과 반환
