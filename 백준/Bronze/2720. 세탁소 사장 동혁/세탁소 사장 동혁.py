f601e3533b907e6dc52feb246b9cc786dd793ba6
yeahhh1e <148204348+yeahhh1e@users.noreply.github.com>
2025-08-12 20:18:54 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 60 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -1,23 +1,12 @@
 T = int(input())
-for _ in range(1, T+1):
+coins = [25, 10, 5, 1]
+
+for _ in range(T):
     C = int(input())
-    
-    q = 0
-    d = 0
-    n = 0
-    p = 0
+    result = []
 
-    q = C // 25
-    C = C % 25
-    
-    d = C // 10
-    C = C % 10
-    
-    n = C // 5
-    C = C % 5
-
-    p = C // 1
-    C = C % 1
-
-    result = [q, d, n, p]
+    for coin in coins:
+        result.append(C // coin)
+        C = C % coin
+        
     print(*result)
