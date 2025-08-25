0f218be8b12c22c68b004c4940a21ba8581cc97a
yeahhh1e <148204348+yeahhh1e@users.noreply.github.com>
2025-08-25 19:24:42 +0900

    [Bronze I] Title: 소가 길을 건너간 이유 1, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/14467. 소가 길을 건너간 이유 1/소가 길을 건너간 이유 1.py
+++ 백준/Bronze/14467. 소가 길을 건너간 이유 1/소가 길을 건너간 이유 1.py
@@ -4,9 +4,10 @@ di = {}
 cnt = 0
 
 for i in li:
-    if i[0] not in di.keys():
+    if not di.get(i[0]):
         di[i[0]] = i[1]
     elif di[i[0]] != i[1]:
         cnt += 1
         di[i[0]] = i[1]
+
 print(cnt)
